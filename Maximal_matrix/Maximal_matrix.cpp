3c66c8140d39a8e4ab0103ea2258b1002c05b298
Vladislav <kuznetsov230304@gmail.com>
2022-12-02 21:57:16 +0500

    Добавьте файлы проекта.

---
+++ Maximal_matrix/Maximal_matrix.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M ;
+    double** matrix = new double*[N];
+    for (int i = 0; i < N; ++i)
+        matrix[i] = new double[M];
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+
+    double maxis = numeric_limits<double>::min();
+    double minis = numeric_limits<double>::max();
+
+
+    for (int i = 0; i < N; ++i) {
+        int r = -1;
+        int c = -1;
+        for (int j = 0; j < M; ++j)
+        {
+            if (matrix[i][j] > maxis)
+            {
+                maxis = matrix[i][j];
+                c = i;
+                r = j;
+            }
+        }
+
+        for (int j = 0; j < N; ++j)
+        {
+            if (matrix[j][r] < minis)
+            {
+                minis = matrix[j][r];
+            }
+        }
+
+        if (maxis==minis && c>=0 && r>=0)
+        {
+            cout << c << " " << r << endl;
+        }
+    }
+
+
+
+    return 0;
+}
